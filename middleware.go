141e64298eb5551f3141b8ebe067da951ae1d874
Michael Forman <mforman@gmail.com>
2016-01-09 15:34:21 -0500

    Remove the logging handler since heroku's log is good enough

--- middleware.go
+++ middleware.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"time"
 )
 
 func optionsOk(next http.Handler) http.Handler {
@@ -29,17 +27,6 @@ func contentTypeJsonHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
-func loggingHandler(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		t1 := time.Now()
-		next.ServeHTTP(w, r)
-		t2 := time.Now()
-		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
-	}
-
-	return http.HandlerFunc(fn)
-}
-
 func commonHandlers(next http.HandlerFunc) http.Handler {
-	return loggingHandler(contentTypeJsonHandler(optionsOk(next)))
+	return contentTypeJsonHandler(optionsOk(next))
 }
